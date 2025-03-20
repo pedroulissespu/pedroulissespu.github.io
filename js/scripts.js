dd1fa27e719221d5946f84215db382e99d0d0e38
Pedro Ulisses <pedro.ulisses@aluno.uece.br>
2025-03-20 11:38:06 -0300

    Add blog, header, footer, about, and experience sections to the portfolio

---
+++ js/scripts.js
@@ -0,0 +1,39 @@
+const loadPartial = (id, file) => {
+    fetch(file)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`Erro ao carregar ${file}: ${response.statusText}`);
+            }
+            return response.text();
+        })
+        .then(data => document.getElementById(id).innerHTML = data)
+        .catch(error => console.error(error.message));
+};
+
+loadPartial('header', 'partials/header.html');
+loadPartial('sobre', 'partials/sobre.html');
+loadPartial('projetos', 'partials/projetos.html');
+loadPartial('conhecimentos', 'partials/conhecimentos.html');
+loadPartial('certificacoes', 'partials/certificacoes.html');
+loadPartial('experiencia', 'partials/experiencia.html');
+loadPartial('blog', 'partials/blog.html');
+loadPartial('footer', 'partials/footer.html');
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const targetElement = document.querySelector(this.getAttribute('href'));
+        if (targetElement) {
+            targetElement.scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            });
+        }
+    });
+});
+
+document.querySelectorAll('div[id]').forEach(div => {
+    div.addEventListener('error', () => {
+        console.error(`Erro ao carregar o conteúdo de ${div.id}`);
+    });
+});
